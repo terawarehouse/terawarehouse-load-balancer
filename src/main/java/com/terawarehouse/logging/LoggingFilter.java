d35d73c7eacd22a68de5f1c66d7264c6b88401c6
Edward P. Legaspi <czetsuya@gmail.com>
2019-11-02 10:48:14 +0800

    Update readme.

---
+++ src/main/java/com/terawarehouse/logging/LoggingFilter.java
@@ -0,0 +1,60 @@
+/**
+ * An Open Source Inventory and Sales Management System
+ * Copyright (C) 2019 Edward P. Legaspi (https://github.com/czetsuya)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.terawarehouse.logging;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+/**
+ * @author Edward P. Legaspi | czetsuya@gmail.com
+ */
+@Component
+public class LoggingFilter extends ZuulFilter {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        logger.info("request={}, user={}, requestUri={}", request, request.getUserPrincipal(), request.getRequestURI());
+
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+}
